ca2227cb75475e215b0307dabea8774fb4d42854
luke jackson <lukeajackson98@gmail.com>
2021-12-15 18:44:19 +1100

    added CGAL and boost library

---
+++ cgal_test/CMakeFiles/cgal_implicit_sphere.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cgal_implicit_sphere.dir/cgal_implicit_sphere.cpp.o"
+  "cgal_implicit_sphere"
+  "cgal_implicit_sphere.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cgal_implicit_sphere.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
