0c7f02ceecdf95caf8df155909fe6d635dcc3769
luke jackson <lukeajackson98@gmail.com>
2021-12-21 21:14:58 +1100

    removed build, install, log files

--- redback_ws/build/CGAL/CGALConfig.cmake
+++
@@ -1,5 +0,0 @@
-# 
-# This file points to the CGALConfig.cmake for header-only CGAL.
-#
-
-include(/home/redback/Redback_ControlSystems/redback_ws/src/cgal/Installation/lib/cmake/CGAL/CGALConfig.cmake)
--- redback_ws/build/CGAL/CGALConfigVersion.cmake
+++
@@ -1,5 +0,0 @@
-# 
-# This file points to the CGALConfigVersion.cmake for header-only CGAL.
-#
-
-include(/home/redback/Redback_ControlSystems/redback_ws/src/cgal/Installation/lib/cmake/CGAL/CGALConfigVersion.cmake)
